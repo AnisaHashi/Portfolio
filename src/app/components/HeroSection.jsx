2f582bb774a4135c8634ec37ff48dc194999c2cd
Anisa Hashi <aniso1020@gmail.com>
2024-10-08 22:20:52 +0200

    Start Hero Section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+
+export const HeroSection = () => {
+  return (
+    <section>
+       <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self.center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I am Anisa
+          </h1>
+
+          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            sapiente voluptatem eos, non tempora similique minima nemo?
+          </p>
+          <div>
+            <button>Hire Me</button>
+          </div>
+
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px relative">
+            <Image
+            src="/images/hero-image.png"
+            alt="hero image"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width={300}
+            height={500}
+            
+            
+            />
+            </div>
+           
+        </div>
+      </div>
+    </section>
+  );
+};
